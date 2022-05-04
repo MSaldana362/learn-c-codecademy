67100a9682cebe723a91f5648e2f174d40603a7c
linus <msaldana362@gmail.com>
2022-05-03 20:35:40 -0700

    Updates to Lesson 04

--- lesson_04.c
+++ lesson_04.c
@@ -55,4 +55,50 @@ int main(){
     } else {
         printf("\tSo a is zero\n");
     }
+
+
+    // switch Statement
+    int number = 10;
+    printf("\nMy number is %i\n",number);
+
+    /*
+    The break keyword tells the computer to exit the block
+    and not execute any more code or check any other cases
+    inside the code block.
+    */
+
+    switch (number)
+    {
+    case 1:
+        printf("\tNumber is 1\n");
+        break;
+    
+    case 0:
+        printf("\tNumber is 0\n");
+        break;
+    
+    default:
+        printf("\tNumber is neither 1 or 0\n");
+        break;
+    }
+
+    /*
+    At the end of each switch statement there is a default statement.
+    If none of the cases are true, then the code in the default
+    statment will run.
+    */
+
+
+    // Ternary Operators
+    int x = 10;
+    int y = 20;
+    printf("\nx = %i and y = %i\n",x,y);
+
+    /* Structure for ternary operator:
+    condition ? do something : do something else; */
+
+    int max = (x>y) ? x : y;
+    printf("\tMax value is %i\n",max);
+
+    (x>y) ? printf("\tSo x is greater than y\n") : printf("\tSo y is greater than x\n");
 }
