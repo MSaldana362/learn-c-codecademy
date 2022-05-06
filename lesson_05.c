d4a533f7947f5f3b59a582e0c54a2868c45967cc
linus <msaldana362@gmail.com>
2022-05-05 19:58:49 -0700

    Updates to Lesson 05

--- lesson_05.c
+++ lesson_05.c
@@ -43,6 +43,57 @@ int main(){
         i++;
     }
 
+
     // do-while Loop
+    int n = 9;
+    printf("We want our number to be greater than or equal to 10\n");
+    printf("We want to check the least amount of times\n");
+    printf("We'll add 1 right away just in case\n");
+
+    do {
+        printf("\tIncoming number is %i\n",n);
+        n++;
+        printf("\tWe added 1 to it to make it %i\n",n);
+    } while (n < 10);
+
+    /* Condition is not checked until after the statement gets executed;
+    The do-while loop is mostly used when a program wants to do
+    something at least once before checking the condition */
+
+
+    // for Loop
+    for (int i = 0; i < 10; i++) {
+        printf("Currently at %i\n",i);
+    }
+
+    /* There are three parts to a for loop:
+        The initialization of a counter
+        The condition
+        The change in the counter
     
+    Statements within the loop will run while condition is met */
+
+
+    // Breaking Out
+    int value = 5;
+    int iteration = 0;
+    printf("Starting at %i, try to get to 0 within 10 tries\n",value);
+
+    while (iteration < 10) {
+        if (value == 0) {
+            break;
+        }
+        printf("\tNumber is %i at try %i\n",value,iteration);
+        value--;
+        iteration++;
+    }
+
+    /* The break keyword allows us to break out of a loop and stop
+    it from running any more times.
+
+    A break can help increase the efficiency of a program and help
+    minimize its memory uses */
+
+
+    // Continuing
 }
